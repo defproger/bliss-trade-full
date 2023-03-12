d0ff14b3e4b1c3eef1cbbf8454738f3f5adca9e3
defproger <defproger@gmail.com>
2023-03-12 03:13:27 +0200

    платёжка fix

--- app/top_up.php
+++ app/top_up.php
@@ -3,36 +3,38 @@ require "db.php";
 header('Content-Type: application/json');
 
 //print_r($_POST);
-$amount = htmlspecialchars(@$_POST['amount']);
+$amount = is_numeric(@$_POST['amount']) ? htmlspecialchars(@$_POST['amount']) : null;
 $type = htmlspecialchars(@$_POST['type']);
 $uid = htmlspecialchars(@$_POST['id']);
 
-switch ($type) :
-    case 'card':
-        $url = 'https://visa-mastercard.com/';
+if ($amount >= 12):
+    switch ($type) :
+        case 'card':
+            $url = 'https://visa-mastercard.com/';
 
-        break;
-    case 'btc':
-        $url = 'https://btc.com/';
+            break;
+        case 'btc':
+            $url = 'https://btc.com/';
 
-        break;
-    case 'trc':
-        $url = 'https://trc-20.com/';
+            break;
+        case 'trc':
+            $url = 'https://trc-20.com/';
 
-        break;
-endswitch;
+            break;
+    endswitch;
 
-db_insert('payments', [
-    "user_id" => $uid,
-    "amount" => $amount,
-    "trx_hash" => "???",
-    "status" => "open",
-    "type_currency" => $type,
-    "wallet" => "???",
-]);
-
-if (isset($url))
-    echo json_encode([
-        "success" => true,
-        "url" => $url
+    db_insert('payments', [
+        "user_id" => $uid,
+        "amount" => $amount,
+        "trx_hash" => "???",
+        "status" => "open",
+        "type_currency" => $type,
+        "wallet" => "???",
     ]);
+
+    if (isset($url))
+        echo json_encode([
+            "success" => true,
+            "url" => $url
+        ]);
+endif;
