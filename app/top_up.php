b6f0bc8259ede1ee67322f0e0aa5c0d7460ffe4c
defproger <defproger@gmail.com>
2023-03-12 03:05:24 +0200

    платёжка example

--- app/top_up.php
+++ app/top_up.php
@@ -1,9 +1,38 @@
 <?php
-
+require "db.php";
 header('Content-Type: application/json');
 
 //print_r($_POST);
-echo json_encode([
-    "success" => true,
-    "url" => 'https://rt.pornhub.com/'
+$amount = htmlspecialchars(@$_POST['amount']);
+$type = htmlspecialchars(@$_POST['type']);
+$uid = htmlspecialchars(@$_POST['id']);
+
+switch ($type) :
+    case 'card':
+        $url = 'https://visa-mastercard.com/';
+
+        break;
+    case 'btc':
+        $url = 'https://btc.com/';
+
+        break;
+    case 'trc':
+        $url = 'https://trc-20.com/';
+
+        break;
+endswitch;
+
+db_insert('payments', [
+    "user_id" => $uid,
+    "amount" => $amount,
+    "trx_hash" => "???",
+    "status" => "open",
+    "type_currency" => $type,
+    "wallet" => "???",
 ]);
+
+if (isset($url))
+    echo json_encode([
+        "success" => true,
+        "url" => $url
+    ]);
